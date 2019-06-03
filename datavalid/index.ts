1957ab42e3abbbf0b5ff3361d582e9cc0fd49bce
Lucas Santos <lhs.santoss@gmail.com>
2019-06-03 15:23:41 -0300

    Add barrel

---
+++ datavalid/index.ts
@@ -0,0 +1,6 @@
+export * from './dataAndImageInput'
+export * from './dataAndImageOutput'
+export * from './dataOnlyInput'
+export * from './dataOnlyOutput'
+export * from './imageOnlyInput'
+export * from './imageOnlyOutput'
